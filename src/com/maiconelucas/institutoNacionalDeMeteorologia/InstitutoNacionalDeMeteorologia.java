4b7c2c5b0d12f27e1ba504a49f7ce53fe4af49a7
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2021-03-02 13:01:24 -0300

    Update InstitutoNacionalDeMeteorologia.java

--- src/com/maiconelucas/institutoNacionalDeMeteorologia/InstitutoNacionalDeMeteorologia.java
+++ src/com/maiconelucas/institutoNacionalDeMeteorologia/InstitutoNacionalDeMeteorologia.java
@@ -1,25 +1,48 @@
 package com.maiconelucas.institutoNacionalDeMeteorologia;
 
+import java.util.Scanner;
+
 public class InstitutoNacionalDeMeteorologia {
 
 public static void main(String[] args) {
 		
 		// testes	
+	    Scanner ler=new Scanner(System.in);
 		LeituraEValidacoes util = new LeituraEValidacoes();
-		
 		util.lerMesAno();
-		System.out.println(util.getNumeroMes());
-		System.out.println(util.getNumeroAno());
-		
-		System.out.println(util.verificaBissexto(util.getNumeroAno()));
-		System.out.println("o tamanho do mes eh: " + util.obtemTamanhoMes());
-		System.out.println(util.getTamanhoMes());
-		
-		util.lerTemperaturas();
-		System.out.println("A temperatura máxima foi:"+util.getTemperaturaMaxima());
-		System.out.println("A temperatura mínima foi:"+util.getTemperaturaMinima());
-		System.out.println("A temperatura média foi:"+util.getTemperaturaMedia());
-	
+		util.getNumeroMes();
+		util.getNumeroAno();
+	    util.verificaBissexto(util.getNumeroAno());
+	    util.obtemTamanhoMes();
+	    util.getTamanhoMes();
+	    util.lerTemperaturas();
+	    
+	    Painel painel=new Painel();
+	    painel.apresentaSegundoMenu();
+	   int i=0;
+	   for(i=0;i<6;i++) {
+		   
+		   
+		   if(i==1) {
+			   painel.apresentaTemperaturaMedia(util.getNumeroMes(), util.getTemperaturaMedia());
+		   }
+		   else if(i==2) {
+			   painel.apresentaTemperaturaMinima(util.getNumeroMes(), util.getTemperaturaMinima());
+		   }
+		   else if(i==3) {
+			   painel.apresentaTemperaturaMaxima(util.getNumeroMes(), util.getTemperaturaMaxima());
+		   }
+		   else if(i==4) {
+			   painel.apresentaRelatorio(util.getNumeroMes(), util.getTemperaturaMedia(), util.getTemperaturaMinima(), util.getTemperaturaMaxima());
+		   }
+		   else if(i==5) {
+			   painel.apresentaVoceSaiu();
+		   }
+		   else {
+			   painel.apresentaOpcaoInvalida();
+		   }
+	   }
+	    
 	}
 
 }
